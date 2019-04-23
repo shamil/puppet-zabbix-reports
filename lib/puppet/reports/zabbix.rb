c1cb6c70de8e872d22ed56070cbf538bf9d85d9f
Becca Robinson <becca@puppet.com>
2019-04-23 16:36:00 -0500

    Updated ruby code to pass rubocop validation with pdk.

--- lib/puppet/reports/zabbix.rb
+++ lib/puppet/reports/zabbix.rb
@@ -2,20 +2,20 @@ require 'puppet'
 require 'puppet/util/zabbix_sender'
 
 Puppet::Reports.register_report(:zabbix) do
-  desc "Send reports to a Zabbix server via zabbix trapper."
+  desc 'Send reports to a Zabbix server via zabbix trapper.'
 
   def process
-    configfile = File.join([File.dirname(Puppet.settings[:config]), "zabbix.yaml"])
+    configfile = File.join([File.dirname(Puppet.settings[:config]), 'zabbix.yaml'])
     raise Puppet::ParseError, "zabbix report config file #{configfile} not readable" unless File.exist?(configfile)
 
     config = YAML.load_file(configfile)
-    default_zabbix_port = config.fetch(:zabbix_port, 10051)
+    default_zabbix_port = config.fetch(:zabbix_port, '10051')
     host_overrides = config.fetch(:host_overrides, {})
 
     zabbix_hosts = [] # instantiate an empty array
-    zabbix_hosts << {'address' => config[:zabbix_host]} if config[:zabbix_host] # for backward cimpatibility
-    zabbix_hosts = zabbix_hosts + config[:zabbix_hosts] if config[:zabbix_hosts]
-    raise Puppet::ParseError, "zabbix host(s) must be specified in config file" unless zabbix_hosts.length > 0
+    zabbix_hosts << { 'address' => config[:zabbix_host] } if config[:zabbix_host] # for backward cimpatibility
+    zabbix_hosts += config[:zabbix_hosts] if config[:zabbix_hosts]
+    raise Puppet::ParseError, 'zabbix host(s) must be specified in config file' unless zabbix_hosts.!empty?
 
     raise_error = false
     zabbix_hosts.each do |zhost|
@@ -23,25 +23,25 @@ Puppet::Reports.register_report(:zabbix) do
       zabbix_sender = Puppet::Util::Zabbix::Sender.new zhost['address'], port
 
       # simple info
-      zabbix_sender.add_item "puppet.version", self.puppet_version
-      zabbix_sender.add_item "puppet.run.timestamp", self.time.to_i
+      zabbix_sender.add_item 'puppet.version', puppet_version
+      zabbix_sender.add_item 'puppet.run.timestamp', time.to_i
 
       # collect metrics
-      self.metrics.each do |metric, data|
+      metrics.each do |metric, data|
         next if metric == 'events' # do not process events at all
 
         data.values.each do |item|
-          next if metric == 'time' and item.first != 'total' # get only total time
+          next if metric == 'time' && item.first != 'total' # get only total time
           zabbix_sender.add_item "puppet.#{metric}.#{item.first}", item.last
         end
       end
 
       # send metrics to zabbix
-      Puppet.debug "sending zabbix report for host #{self.host}, at #{zabbix_sender.serv}:#{zabbix_sender.port}"
+      Puppet.debug "sending zabbix report for host #{host}, at #{zabbix_sender.serv}:#{zabbix_sender.port}"
       begin
-        result = zabbix_sender.send! host_overrides.fetch(self.host, self.host)
+        result = zabbix_sender.send! host_overrides.fetch(host, host)
       rescue => e
-        result = {'info' => e}
+        result = { 'info' => e }
       end
 
       # validate the response. if it fails, keep on sending to all
@@ -52,6 +52,6 @@ Puppet::Reports.register_report(:zabbix) do
       end
     end
 
-    raise Puppet::Error, "zabbix report had failures" if raise_error
+    raise Puppet::Error, 'zabbix report had failures' if raise_error
   end
 end
--- lib/puppet/util/zabbix_sender.rb
+++ lib/puppet/util/zabbix_sender.rb
@@ -14,14 +14,15 @@ module Puppet::Util::Zabbix
     # Puppet::Util::Zabbix::Sender.send 'host', 'zabbix_host', 10051 do
     #   add_item 'key', 'value'
     # end
-    def self.send(host, serv = 'localhost', port = 10051, &blk)
+    def self.send(host, serv = 'localhost', port = '10051', &blk)
       s = new serv, port
       s.instance_eval(&blk)
       s.send! host
     end
 
-    def initialize(serv = 'localhost', port = 10051)
-      @serv, @port = serv, port
+    def initialize(serv = 'localhost', port = '10051')
+      @serv = serv
+      @port = port
       @items = {}
     end
 
@@ -32,9 +33,9 @@ module Puppet::Util::Zabbix
     def send!(host)
       return if @items.empty?
       begin
-        connect @items.map { |key, value|
-            { :host => host.to_s, :key => key.to_s, :value => value.to_s }
-        }
+        connect(@items.map) do |key, value|
+          { host: host.to_s, key: key.to_s, value: value.to_s }
+        end
       ensure
         @items = {}
       end
@@ -47,7 +48,7 @@ module Puppet::Util::Zabbix
       begin
         sock = TCPSocket.new @serv, @port
         sock.write rawdata(data)
-        JSON.parse sock.read[13 .. -1]
+        JSON.parse sock.read[13..-1]
       ensure
         sock.close if sock
       end
@@ -56,12 +57,11 @@ module Puppet::Util::Zabbix
     def rawdata(data)
       data = [data] unless data.instance_of? Array
       baggage = {
-        :request => 'sender data',
-        :data    => data,
+        request: 'sender data',
+        data: data,
       }.to_json
 
-      "ZBXD\1" + [baggage.bytesize].pack("i") + "\0\0\0\0" + baggage
+      "ZBXD\1" + [baggage.bytesize].pack('i') + "\0\0\0\0" + baggage
     end
-
   end
 end
